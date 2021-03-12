4d2c78d5c2f142c0af71602ad8d6b730ebd79255
djou20 <julianhofmann67@gmail.com>
2021-03-12 15:04:42 +0100

    - Added internationalization

--- lib/components/StoresScreenTabs/StoreListScreen.dart
+++ lib/components/StoresScreenTabs/StoreListScreen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 // import 'package:shopisan/components/StoreList/List.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StoreListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Center(child: Text("Page contenant la liste des stores"),),
+        child: Center(child: Text(AppLocalizations.of(context).helloWorld),),
         // child: StoreList()//,
     );
   }
