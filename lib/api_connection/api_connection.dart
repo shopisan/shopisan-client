ecb42ed49b58b17e68e881d541641d9f93ef1c3c
djou20 <julianhofmann67@gmail.com>
2021-04-09 15:37:42 +0200

    - Data display - Bug fix (+ new bugs created)

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -15,8 +15,8 @@ import 'package:shopisan/repository/user_repository.dart';
 part 'kits/posts_api.dart';
 part 'kits/user_api.dart';
 
-final _base = "10.0.2.2:8000";
-// final _base = "shopisan.herokuapp.com";
+// final _base = "10.0.2.2:8000";
+final _base = "shopisan.herokuapp.com";
 final _tokenEndpoint = "/api/token-auth/";
 final _tokenURL = Uri.http(_base, _tokenEndpoint);
 
