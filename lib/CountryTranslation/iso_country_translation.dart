b4dd441394b2863fb756a51acc6b599f778dc977
alyson <alyson@jh8.dev>
2021-04-19 10:21:56 +0200

    form - posts - store

--- lib/CountryTranslation/iso_country_translation.dart
+++ lib/CountryTranslation/iso_country_translation.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
+
 import 'country_translations.dart';
 
 // todo faire la fonction pour traduire les iso selon la locale
-class IsoCountryTranslation{
+class IsoCountryTranslation {
   static String defaultLocale = Platform.localeName;
 
-  static String getCountryName(String isoCode){
+  static String getCountryName(String isoCode) {
     final String localeCode = getLocaleCode();
     return countryTranslations[localeCode][isoCode.toLowerCase()];
 
     // todo selon la locale check dans le bon fichier + return la traduction
   }
 
-  static getLocaleCode(){
+  static getLocaleCode() {
     if (defaultLocale.contains("fr")) {
       return "fr";
     }
